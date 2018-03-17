67d7e3bcaca837e287f3ec666f548b67d2faa12e
Blair Urish <burace17@gmail.com>
2018-03-17 00:08:37 -0500

    added kansas precinct layer and modified widget to show attributes from layer

--- app/main.ts
+++ app/main.ts
@@ -8,9 +8,7 @@ import Query = require("esri/tasks/support/Query");
 import QueryTask = require("esri/tasks/QueryTask");
 import PrecinctInfo = require("app/precinctinfo");
 
-const map = new EsriMap( {
-    basemap: "streets" as any as Basemap
-});
+const map = new EsriMap();
 
 const view = new MapView({
     map: map,
@@ -22,22 +20,35 @@ const view = new MapView({
     zoom: 12
 });
 
-const testLayer = new FeatureLayer({
-    url: "https://services8.arcgis.com/yBvhbG6FeRtNxtFh/arcgis/rest/services/CA_31_2016/FeatureServer",
+const precinctsLayer = new FeatureLayer({
+    url: "https://services8.arcgis.com/yBvhbG6FeRtNxtFh/arcgis/rest/services/KansasVTDs2010/FeatureServer",
     outFields: ["*"]
 });
 
-var pi = new PrecinctInfo({
+var info = new PrecinctInfo({
     container: "widgetDiv"
 });
 
-view.on("pointer-move", function(event) {
-    view.hitTest(event).then(function (response) {
+view.on("pointer-move", (event) => {
+    view.hitTest(event).then((response) => {
         const graphic = response.results[0].graphic;
         const attributes = graphic.attributes;
-        pi.precinctName = attributes.ca31_results_csv_name;
+        info.precinctName = attributes.VTDNAME;
+        info.population = attributes.POPULATION;
+        info.white = attributes.WHITE;
+        info.black = attributes.BLACK;
+        info.hispanic = attributes.HISPANIC_O;
+        info.asian = attributes.ASIAN;
+        info.amindian = attributes.AMINDIAN;
+
+        info.obama = attributes.PRES_08_DE;
+        info.mccain = attributes.PRES_08_RE;
+        info.other = attributes.PRES_08_OT;
     });
 });
 
-map.add(testLayer);
-view.ui.add(pi);
+map.add(precinctsLayer);
+precinctsLayer.queryExtent().then((response) => {
+    view.goTo(response.extent);
+});
+view.ui.add(info, { position: "top-right",  index:0});
--- app/precinctinfo.tsx
+++ app/precinctinfo.tsx
@@ -12,10 +12,57 @@ class PrecinctInfo extends declared(Widget) {
     @renderable()
     precinctName: string = "Test";
 
+    @property()
+    @renderable()
+    population: number = 0;
+
+    @property()
+    @renderable()
+    white: number = 0;
+
+    @property()
+    @renderable()
+    black: number = 0;
+
+    @property()
+    @renderable()
+    amindian: number = 0;
+
+    @property()
+    @renderable()
+    asian: number = 0;
+
+    @property()
+    @renderable()
+    hispanic: number = 0;
+
+    @property()
+    @renderable()
+    obama: number = 0;
+
+    @property()
+    @renderable()
+    mccain: number = 0;
+
+    @property()
+    @renderable()
+    other: number = 0;
+
     render() {
         return (
             <div bind={this} class={CSS.base}>
-            {this.precinctName}
+            {this.precinctName}<br />
+            Population: {this.population}<br />
+            <hr />
+            White: {this.white}<br />
+            Black: {this.black}<br />
+            Hispanic: {this.hispanic}<br />
+            Asian: {this.asian}<br />
+            AmIndian: {this.amindian}<br />
+            <hr />
+            Obama: {this.obama}<br />
+            McCain: {this.mccain}<br />
+            Other: {this.other}<br />
             </div>
         )
     }
