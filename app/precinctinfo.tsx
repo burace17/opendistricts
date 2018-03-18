d426e6074b0c1ac1af2821569c3ea9c56744c733
Blair Urish <burace17@gmail.com>
2018-03-18 15:46:22 -0500

    added percentages for each attribute

--- app/precinctinfo.tsx
+++ app/precinctinfo.tsx
@@ -48,21 +48,34 @@ class PrecinctInfo extends declared(Widget) {
     @renderable()
     other: number = 0;
 
+    getPercentage(n: number, total: number) {
+        if (total == 0) {
+            return ""
+        }
+        else {
+            return "(" + ((n / total)*100).toFixed(1) + "%)"
+        }
+    }
+
+    getPresidentialTotal() {
+        return this.obama + this.mccain + this.other
+    }
+
     render() {
         return (
             <div bind={this} class={CSS.base}>
             {this.precinctName}<br />
             Population: {this.population}<br />
             <hr />
-            White: {this.white}<br />
-            Black: {this.black}<br />
-            Hispanic: {this.hispanic}<br />
-            Asian: {this.asian}<br />
-            AmIndian: {this.amindian}<br />
+            White: {this.white} {this.getPercentage(this.white, this.population)}<br />
+            Black: {this.black} {this.getPercentage(this.black, this.population)}<br />
+            Hispanic: {this.hispanic} {this.getPercentage(this.hispanic, this.population)}<br />
+            Asian: {this.asian} {this.getPercentage(this.asian, this.population)}<br />
+            AmIndian: {this.amindian} {this.getPercentage(this.amindian, this.population)}<br />
             <hr />
-            Obama: {this.obama}<br />
-            McCain: {this.mccain}<br />
-            Other: {this.other}<br />
+            Obama: {this.obama} {this.getPercentage(this.obama, this.getPresidentialTotal())}<br />
+            McCain: {this.mccain} {this.getPercentage(this.mccain, this.getPresidentialTotal())}<br />
+            Other: {this.other} {this.getPercentage(this.other, this.getPresidentialTotal())}<br />
             </div>
         )
     }
