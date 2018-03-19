c31feb626edfb16f98a2361b5a0e92a7d838c314
Blair Urish <burace17@gmail.com>
2018-03-18 22:09:45 -0500

    district info widget now shows districts with their deviation from the ideal population value

--- app/districtinfo.tsx
+++ app/districtinfo.tsx
@@ -6,6 +6,22 @@ const CSS = {
     base: "district-info"
 };
 
+class District  {
+    num: number = 0;
+    population: number = 0;
+    idealPopulation: number = 0;
+
+    constructor(n: number, idealPop: number) {
+        this.num = n;
+        this.idealPopulation = idealPop;
+    }
+
+    @property({readOnly: true, dependsOn:["population"]})
+    get deviation() {
+        return this.population - this.idealPopulation;
+    }
+}
+
 @subclass("esri.widgets.DistrictInfo")
 class DistrictInfo extends declared(Widget) {
     @property()
@@ -16,14 +32,29 @@ class DistrictInfo extends declared(Widget) {
     @renderable()
     numberOfDistricts: number = 0
 
+    @property()
+    @renderable()
+    districts: Array<District> = []
+
+    private updateDistrictArray() {
+        this.districts = []
+        var idealPop = Math.floor(this.population / this.numberOfDistricts);
+        for (let i = 0; i < this.numberOfDistricts; i++)
+        {
+            this.districts.push(new District(i+1, idealPop));
+        }
+    }
+
     private removeDistrict() {
         if (this.numberOfDistricts > 0) {
             this.numberOfDistricts--;
+            this.updateDistrictArray();
         }
     }
 
     private addDistrict() {
         this.numberOfDistricts++;
+        this.updateDistrictArray();
     }
 
     render() {
@@ -31,8 +62,10 @@ class DistrictInfo extends declared(Widget) {
             <div bind={this} class={CSS.base}>
                 State Population: {this.population.toLocaleString()}<br />
                 Districts: <button bind={this} onclick={this.removeDistrict}>-</button> {this.numberOfDistricts} <button bind={this} onclick={this.addDistrict}>+</button>
+                <hr />
+                {this.districts.map((district) => <span key={district.num}>{district.num} {district.deviation} <br /> </span>)}
             </div>
-        )
+        )   
     }
 }
 
